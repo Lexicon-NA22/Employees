792dcd7661707a383e0a02e95fd4781666d721c3
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-12-01 15:21:34 +0100

    Add Util

---
+++ Employees/Util.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    public static class Util
+    {
+        public static string AskForString(string prompt)
+        {
+            bool success = false;
+            string name;
+
+            do
+            {
+                Console.WriteLine($"{prompt}: ");
+                name = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    Console.WriteLine($"You must enter a {prompt}");
+                }
+                else
+                {
+                    success = true;
+                }
+
+            } while (!success);
+
+            return name;
+        }
+    }
+}
