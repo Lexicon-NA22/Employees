f4add922eb16da7deb55b6520e721a7be5b1c475
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-12-01 13:23:37 +0100

    Robot - properties

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -6,7 +6,34 @@ namespace Employees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Robot robot = new Robot(100, "Kalle", 25);
+            Robot robot2 = new Robot(50, "Eva", 20);
+            //Robot r = new Robot();
+            //robot.SetName("Nisse");
+            //string name = robot.GetName();
+            
+              robot.Age = -11;
+            //try
+            //{
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Wrong age " + e.Message);
+            //}
+
+            int[] arr = { 2, 3, 4, 5, 6, 7, 8 };
+
+            int x = arr[100];
+
+            robot = null;
+
+            var age = robot?.Age;
+
+            //int age = robot2.Age;
+
+            //robot.Age2 = 25;
+            //var age2  = robot2.Age2;
+            
         }
     }
 }
---
+++ Employees/Robot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Employees
+{
+    public class Robot
+    {
+        private string name;
+        private int age;
+
+        public int Height { get; set; }
+        public int Age
+        {
+            get { return age; }
+            set 
+            { 
+                if(value < 0)
+                {
+                    age = 0;
+                   // throw new ArgumentException(nameof(age));
+                }
+                else
+                {
+                    age = value;
+                }
+            
+            }
+        }
+
+        public Robot(int height, string name, int age)
+        {
+            Height = height;
+            this.name = name;
+            Age = age;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string newName)
+        {
+            //Validate
+            name = newName;
+        }
+    }
+}
